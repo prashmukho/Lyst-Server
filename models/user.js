8d2164a8ee2b565380da4a2295fc006badb66345
prashmukho <prashantmukhopadhyay@gmail.com>
2016-05-30 10:29:34 +0530

    Modified User model schema

--- models/user.js
+++ models/user.js
@@ -1,12 +1,25 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-// mongoose.connect('mongodb://localhost/ionic-test');
-
 var userSchema = new Schema({
-  name: String,
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true }
+  name: { type: String, default: 'New User' },
+
+  username: { 
+    type: String, 
+    required: [true, 'Username is required!'],
+    validate: {
+      validator: function (v) {
+        return /\w+@\w+\.\w{2,3}/.test(v);
+      },
+      message: 'Username must be a valid email address!'
+    },
+    index: { unique: true }
+  },
+
+  password: { 
+    type: String, 
+    required: [true, 'Password is required!']
+  }
 });
 
 module.exports = mongoose.model('User', userSchema);
